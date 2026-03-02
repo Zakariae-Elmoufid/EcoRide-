f01252c7b0dda0900001b672d2ddfe82413b3725
zakariae elmoufid <moufidzakarie3@gmail.com>
2026-03-02 13:51:20 +0000

    first commit

---
+++ src/main/java/org/example/ecoride/EcoRideApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecoride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ecoride/EcoRideApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ecoride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
